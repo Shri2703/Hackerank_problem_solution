f2a4cedc7e354d29e91bd0e9619c13a26265665d
Poorna Shri <poornashri2703@gmail.com>
2024-02-29 22:33:51 +0530

    task

---
+++ Problem_107/TheMinionGame.py
@@ -0,0 +1,76 @@
+'''
+Kevin and Stuart want to play the 'The Minion Game'.
+
+Game Rules
+
+Both players are given the same string, .
+Both players have to make substrings using the letters of the string .
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string .
+
+For Example:
+String  = BANANA
+Kevin's vowel beginning word = ANA
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+
+For better understanding, see the image below:
+
+banana.png
+
+Your task is to determine the winner of the game and their score.
+
+Function Description
+
+Complete the minion_game in the editor below.
+
+minion_game has the following parameters:
+
+string string: the string to analyze
+Prints
+
+string: the winner's name and score, separated by a space on one line, or Draw if there is no winner
+Input Format
+
+A single line of input containing the string .
+Note: The string  will contain only uppercase letters: .
+
+Constraints
+
+
+
+Sample Input
+
+BANANA
+Sample Output
+
+Stuart 12
+Note :
+Vowels are only defined as . In this problem,  is not considered a vowel.
+'''
+
+#Solution
+
+def minion_game(string):
+    vowels = 'AEIOU'
+    Stuart_score, Kevin_score = 0, 0
+    length = len(string)
+    for start_idx in range(length):
+        score = length - start_idx
+        if string[start_idx] in vowels:
+            Kevin_score += score
+        else:
+            Stuart_score += score
+    if Stuart_score == Kevin_score:
+        print('Draw')
+    if Stuart_score > Kevin_score:
+        print('Stuart {}'.format(Stuart_score))
+    if Stuart_score < Kevin_score:
+        print('Kevin {}'.format(Kevin_score))
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
